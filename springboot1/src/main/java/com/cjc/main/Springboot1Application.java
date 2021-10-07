a3d798fbd6eff38e1ef07598649bf157ccca2b46
snehal <snehal@DESKTOP-ID5G8H2>
2021-10-07 15:15:44 +0530

    first commit
    uploading all files

---
+++ springboot1/src/main/java/com/cjc/main/Springboot1Application.java
@@ -0,0 +1,15 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1Application {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HELLO");
+		SpringApplication.run(Springboot1Application.class, args);
+	}
+
+}
